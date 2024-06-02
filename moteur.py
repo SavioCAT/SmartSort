5257f41f1835e6a9450f91a15c1d2da0e7511a18
vboxuser <vboxuser@Debian.myguest.virtualbox.org>
2024-06-02 18:12:14 +0200

    adaptation du code pour la pico

--- moteur.py
+++ moteur.py
@@ -1,42 +1,52 @@
 from time import sleep
 from machine import Pin, PWM
 
-class controleur_moteur:
-    def __init__(self):
-        self.pwm_moteur1 = PWM(Pin(16))
-        self.pwm_moteur2 = PWM(Pin(18))
-        self.pwm_moteur3 = PWM(Pin(20))
-        self.pwm_moteur4 = PWM(Pin(22))
+###############################################
+#                                             #
+# Code à destination d'une raspberry pi pico  #
+#                                             #
+###############################################
 
-        #Initialiser des PWM aux bonnes fréquences
-        self.pwm_moteur1.freq(50)
-        self.pwm_moteur2.freq(50)
-        self.pwm_moteur3.freq(50)
-        self.pwm_moteur4.freq(50)
+#initialisation des pins
+pwm_moteur1 = PWM(Pin(16))
+pwm_moteur2 = PWM(Pin(18))
+pwm_moteur3 = PWM(Pin(20))
+pwm_moteur4 = PWM(Pin(22))
 
-        self.reset_moteur()
+pin_comm_pi5_bit0 = Pin(13, Pin.IN, Pin.PULL_DOWN)
+pin_comm_pi5_bit1 = Pin(14, Pin.IN, Pin.PULL_DOWN)
+pin_comm_pi5_declancheur = Pin(15, Pin.IN, Pin.PULL_DOWN)
 
-    #Ce bout de code permet de régler les sortie PWM plus simplement et de ne pas a avoir à gérer la galère qu'est d'utiliser une valeur entre
-    def set_duty(self, moteur: int, duty: float) -> None:
-        try:
-            if duty > 100 or duty < 0 or moteur > 4 or moteur < 1:
-                raise ValueError("duty should be between 0 and 100 and moteur should be an integer betwen 1 and 4")
-            if not isinstance(moteur, int) or not isinstance(duty, float):
-                raise TypeError("duty should be float and moteur should be int")
-            if moteur == 1:
-                self.pwm_moteur1.duty_u16(round(65535 / 100 * duty))
-            elif moteur == 2:
-                self.pwm_moteur2.duty_u16(round(65535 / 100 * duty))
-            elif moteur == 3:
-                self.pwm_moteur3.duty_u16(round(65535 / 100 * duty))
-            elif moteur == 4:
-                self.pwm_moteur4.duty_u16(round(65535 / 100 * duty))
-        except TypeError as e:
-            print(f"TypeErro: {e}")
-        except ValueError as e:
-            print(f"ValueError: {e}")
+#Initialiser des PWM aux bonnes fréquences
+pwm_moteur1.freq(50)
+pwm_moteur2.freq(50)
+pwm_moteur3.freq(50)
+pwm_moteur4.freq(50)
 
-    def reset_moteur(self) -> None:
-        for i in range(1, 5):
-            self.set_duty(i, 50)
 
+def set_duty(moteur: int, duty: float) -> None:
+    try:
+        if duty > 100 or duty < 0 or moteur > 4 or moteur < 1:
+            raise ValueError("duty should be between 0 and 100 and moteur should be an integer betwen 1 and 4")
+        if not isinstance(moteur, int) or not isinstance(duty, float):
+            raise TypeError("duty should be float and moteur should be int")
+        if moteur == 1:
+            pwm_moteur1.duty_u16(round(9830 / 100 * duty))
+        elif moteur == 2:
+            pwm_moteur2.duty_u16(round(9830 / 100 * duty))
+        elif moteur == 3:
+            pwm_moteur3.duty_u16(round(9830 / 100 * duty))
+        elif moteur == 4:
+            pwm_moteur4.duty_u16(round(9830 / 100 * duty))
+    except TypeError as e:
+        print(f"TypeErro: {e}")
+    except ValueError as e:
+        print(f"ValueError: {e}")
+
+
+def reset_moteur() -> None:
+    for i in range(1, 5):
+        set_duty(i, 50)
+
+
+reset_moteur()
