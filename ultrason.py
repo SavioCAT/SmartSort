d5f972ffbbf5ffca212978c012b4977b34c5ac1b
SavioCAT <savio.boissinot@edu.esiee.fr>
2024-06-07 14:52:31 +0200

    ajout de ultrason

---
+++ ultrason.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+class ultrason_logique:
+    def __init__(self):
+        self.GPIO.setmode(GPIO.BOARD)
+        self.GPIO.setwarnings(False)
+
+        self.GPIO.setup(38, GPIO.OUT)
+        self.GPIO.setup(40, GPIO.IN)
+
+        self.GPIO.output(38, GPIO.LOW)
+
+    def distance(self) -> float:
+        self.GPIO.output(38, GPIO.HIGH)
+        sleep(0.00001)
+        self.GPIO.output(38, GPIO.LOW)
+
+        while self.GPIO.input(40) == 0:
+            start = time.time()
+
+        while self.GPIO.input(40) == 1:
+            stop = time.time()
+
+        elapsed = stop - start
+        distance = (elapsed * 34300) / 2
+
+        return distance
